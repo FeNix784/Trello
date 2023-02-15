6bbf2e5bba1f777cc19baf717d252ab57ae23c15
FeNix784 <fenix784@mail.ru>
2023-02-15 23:52:06 +0300

    Создание сущности CommentEntity

--- src/main/java/com/trello/entity/CommentEntity.java
+++ src/main/java/com/trello/entity/CommentEntity.java
@@ -4,11 +4,14 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import java.util.Date;
 
 @Entity
 public class CommentEntity extends PanacheEntity {
 
     public String text;
+
+    public Date date;
 
     @ManyToOne
     public TaskEntity task;
