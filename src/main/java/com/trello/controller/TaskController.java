69af026fc6f1e3203901ddce8ea71dadd28b799e
FeNix784 <fenix784@mail.ru>
2023-02-26 23:32:56 +0300

    Фикс

--- src/main/java/com/trello/controller/TaskController.java
+++ src/main/java/com/trello/controller/TaskController.java
@@ -9,6 +9,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Path("/tasks")
 @Produces(MediaType.APPLICATION_JSON)
